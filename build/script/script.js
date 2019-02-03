30eaab6cf81c6ab321f23ab2b537b897a1c2239e
Damir <k.damir021@gmail.com>
2019-02-03 22:50:34 +0600

    modified

--- build/script/script.js
+++ build/script/script.js
@@ -2,3 +2,8 @@
 $('.button-menu').on('click',function(){
 	$('.link-menu').slideToggle();
 });
+$('.down').on('click',function(event) {
+        event.preventDefault();
+        var siz = $('#bot').offset().top;
+        $('html').animate({scrollTop: siz}, 1000);
+})
